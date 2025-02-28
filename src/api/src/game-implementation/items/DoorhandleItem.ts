0e7ca8337635f5ebf85c70810af698e4e4077256
samale Fahiyeh <samale.fahiyeh@hva.nl>
2025-02-28 15:28:12 +0100

    Me laatste wijzegingen waaronder de

--- src/api/src/game-implementation/items/DoorhandleItem.ts
+++ src/api/src/game-implementation/items/DoorhandleItem.ts
@@ -2,12 +2,22 @@ import { ActionResult } from "../../game-base/actionResults/ActionResult";
 import { TextActionResult } from "../../game-base/actionResults/TextActionResult";
 import { Examine } from "../../game-base/actions/ExamineAction";
 import { Item } from "../../game-base/gameObjects/Item";
+import { gameService } from "../../global";
+import { PickUp } from "../actions/PickUpActions";
+import { PlayerSession } from "../types";
 
-export class DoorHandleItem extends Item implements Examine {
+export class DoorHandleItem extends Item implements Examine, PickUp {
     public static readonly Alias: string = " doorhandle";
 
     public constructor() {
         super(DoorHandleItem.Alias);
+    }
+
+    public pickUp(): string | undefined {
+        const playerSession: PlayerSession = gameService.getPlayerSession();
+        playerSession.pickedupDoorhandle = true;
+
+        return DoorHandleItem.Alias;
     }
 
     public name(): string {
