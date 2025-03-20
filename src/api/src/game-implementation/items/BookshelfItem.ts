4bece0cb560812e87701ab6adde42b085187cc40
Dominik <dominik.krystul@hva.nl>
2025-03-20 13:18:10 +0100

    refactor: Vereenvoudig de examine methode in BookshelfItem door playerSession controles te verwijderen en de tekst te wijzigen

--- src/api/src/game-implementation/items/BookshelfItem.ts
+++ src/api/src/game-implementation/items/BookshelfItem.ts
@@ -2,8 +2,6 @@ import { ActionResult } from "../../game-base/actionResults/ActionResult";
 import { TextActionResult } from "../../game-base/actionResults/TextActionResult";
 import { Examine } from "../../game-base/actions/ExamineAction";
 import { Item } from "../../game-base/gameObjects/Item";
-import { gameService } from "../../global";
-import { PlayerSession } from "../types";
 
 /**
  * Represents the Bookshelf item in the game.
@@ -31,20 +29,9 @@ export class BookshelfItem extends Item implements Examine {
      * @returns {ActionResult | undefined} The result of the examination.
      */
     public examine(): ActionResult | undefined {
-        const playerSession: PlayerSession = gameService.getPlayerSession();
-
-        if (playerSession.inventory.includes("MysteriousStick")) {
-            return new TextActionResult([
-                "It's a bookshelf filled with books.",
-                "You have already found the mysterious stick hidden behind the books.",
-            ]);
-        }
-        else {
-            playerSession.MysteriousStickRevealed = true;
-            return new TextActionResult([
-                "It's a bookshelf filled with books.",
-                "As you examine the books, you notice a mysterious stick hidden behind them.",
-            ]);
-        }
+        return new TextActionResult([
+            "It's a bookshelf filled with books.",
+            "Nothing seems out of the ordinary",
+        ]);
     }
 }
